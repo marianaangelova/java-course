24be8ac514551552e3d7305c4cf7e17643cdd9d9
marianaangelova <mariana37angelova@abv.bg>
2015-07-13 23:28:29 +0300

    hom

--- SoftIntelectFirstHomework/src/FourthClass.java
+++
@@ -1,9 +0,0 @@
-
-public class FourthClass {
-
-	public static void main(String[] args) {
-		String output = "\"I will read the \"Harry Potter\" book collection.\"";
-		System.out.println(output);
-	}
-
-}
